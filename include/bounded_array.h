17249fdfccda1f2144e3d7a8611ebe1252e01806
Ryan Deivert <rdeivert@icloud.com>
2024-07-13 01:16:57 -0700

    fixed bounded array

--- include/bounded_array.h
+++ include/bounded_array.h
@@ -2,18 +2,22 @@
 #include <assert.h>
 
 #define BOUNDED_ARRAY_DEFINE(T, Size) \
-    typedef struct bounded_array_##T { \
+    typedef struct bounded_array_##T##_size_##Size { \
         T data[Size]; \
         size_t length; \
-    } bounded_array_##T; \
+    } bounded_array_##T##_size_##Size; \
     \
-    bounded_array_##T bounded_array_##T##_new(void) { \
-        bounded_array_##T result = {0}; \
+    bounded_array_##T##_size_##Size bounded_array_##T##_size_##Size##_new(void); \
+    void bounded_array_##T##_size_##Size##_append(bounded_array_##T##_size_##Size* array, T entry); \
+
+#define BOUNDED_ARRAY_IMPLEMENTATION(T, Size) \
+    bounded_array_##T##_size_##Size bounded_array_##T##_size_##Size##_new(void) { \
+        bounded_array_##T##_size_##Size result = {0}; \
         result.length = 0; \
         \
         return result; \
     } \
-    void bounded_array_##T##_append(bounded_array_##T* array, T entry) { \
+    void bounded_array_##T##_size_##Size##_append(bounded_array_##T##_size_##Size* array, T entry) { \
         assert(array->length < Size); \
         array->data[array->length] = entry; \
         array->length++; \
