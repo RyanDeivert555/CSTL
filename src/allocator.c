86c9ac6564da1f2b52aed69a316da53a868cbdb6
Ryan Deivert <radeivert@gmail.com>
2025-09-05 18:58:31 -0700

    portability for windows in the std allocator

--- src/allocator.c
+++ src/allocator.c
@@ -9,10 +9,26 @@ void AllocatorRawFree(Allocator allocator, u8* ptr, i64 size, i64 count, i64 ali
     allocator.vtable->free(allocator.ctx, ptr, size, count, align);
 }
 
+static u8* PortableAlignedAlloc(i64 size, i64 count, i64 align) {
+#if defined(_WIN32)
+    return (u8*)_aligned_malloc(size * count, align);
+#else
+    return (u8*)aligned_alloc(align, size * count);
+#endif
+}
+
+static void PortableFree(u8* ptr) {
+#if defined(_WIN32)
+    _aligned_free(ptr);
+#else
+    free(ptr);
+#endif
+}
+
 static u8* StdAlloc(void* ctx, i64 size, i64 count, i64 align) {
     (void)ctx;
 
-    return (u8*)aligned_alloc(align, size * count);
+    return PortableAlignedAlloc(size, count, align);
 }
 
 static void StdFree(void* ctx, u8* ptr, i64 size, i64 count, i64 align) {
@@ -20,7 +36,7 @@ static void StdFree(void* ctx, u8* ptr, i64 size, i64 count, i64 align) {
     (void)size;
     (void)count;
     (void)align;
-    free(ptr);
+    PortableFree(ptr);
 }
 
 static const AllocatorVTable std_allocator_vtable = {
