1bc75d07a4738559cda2841ee38f6e91ff4604ef
Ryan Deivert <radeivert@gmail.com>
2025-09-11 15:47:54 -0700

    removed necessary code

--- src/allocator.c
+++ src/allocator.c
@@ -9,7 +9,9 @@ void AllocatorRawFree(Allocator allocator, u8* ptr, i64 size, i64 count, i64 ali
     allocator.vtable->free(allocator.ctx, ptr, size, count, align);
 }
 
-static u8* PortableAlignedAlloc(i64 size, i64 count, i64 align) {
+static u8* StdAlloc(void* ctx, i64 size, i64 count, i64 align) {
+    (void)ctx;
+
 #if defined(_WIN32)
     return (u8*)_aligned_malloc(size * count, align);
 #else
@@ -17,26 +19,17 @@ static u8* PortableAlignedAlloc(i64 size, i64 count, i64 align) {
 #endif
 }
 
-static void PortableFree(u8* ptr) {
-#if defined(_WIN32)
-    _aligned_free(ptr);
-#else
-    free(ptr);
-#endif
-}
-
-static u8* StdAlloc(void* ctx, i64 size, i64 count, i64 align) {
-    (void)ctx;
-
-    return PortableAlignedAlloc(size, count, align);
-}
-
 static void StdFree(void* ctx, u8* ptr, i64 size, i64 count, i64 align) {
     (void)ctx;
     (void)size;
     (void)count;
     (void)align;
-    PortableFree(ptr);
+
+#if defined(_WIN32)
+    _aligned_free(ptr);
+#else
+    free(ptr);
+#endif
 }
 
 static const AllocatorVTable std_allocator_vtable = {
